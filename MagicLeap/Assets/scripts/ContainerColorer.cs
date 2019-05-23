4b77d6ac033e010a4398e1c5db52561f02f82540
samerboules <samerboules@gmail.com>
2019-05-23 11:11:47 +0200

    Update folder structure

---
+++ MagicLeap/Assets/scripts/ContainerColorer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ContainerColorer
+{
+    public static long GetHexColorFromPrefix(string prefix, bool reefer = false)
+    {
+        if (reefer)
+            return 0xFFFAFAFA;
+
+        long result;
+        if (prefixList.TryGetValue(prefix, out result))
+            return result;
+        else
+            return GetRandomColor();
+    }
+
+    public static long GetRandomColor()
+    {
+        Random rnd = new Random();
+        return prefixList.ElementAt(rnd.Next(prefixList.Count - 1)).Value;
+    }
+
+    private static Dictionary<String, long> prefixList = new Dictionary<String, long>
+    {
+        { "AMFU", 0xFFA2554D },
+        { "APHU", 0xFFA2554D },
+        { "APRU", 0xFF8C9AA6 },
+        { "APZU", 0xFF204190 },
+        { "BEAU", 0xFF204190 },
+        { "BMOU", 0xFF204190 },
+        { "BSIU", 0xFF204190 },
+        { "CAIU", 0xFF204190 },
+        { "CARU", 0xFF023D75 },
+        { "CAXU", 0xFF023D75 },
+        { "CBHU", 0xFF023D75 },
+        { "CCLU", 0xFF023D75 },
+        { "CGMU", 0xFF023D75 },
+        { "CLHU", 0xFF023D75 },
+        { "CMAU", 0xFF023D75 },
+        { "CPSU", 0xFF023D75 },
+        { "CRLU", 0xFF023D75 },
+        { "CRSU", 0xFF023D75 },
+        { "CRXU", 0xFF023D75 },
+        { "CSLU", 0xFF023D75 },
+        { "CSNU", 0xFF023D75 },
+        { "CXDU", 0xFF023D75 },
+        { "CXRU", 0xFF023D75 },
+        { "DFSU", 0xFF023D75 },
+        { "DRYU", 0xFF023D75 },
+        { "DVRU", 0xFF023D75 },
+        { "ECMU", 0xFF023D75 },
+        { "EGHU", 0xFF023D75 },
+        { "EGSU", 0xFF023D75 },
+        { "EISU", 0xFF023D75 },
+        { "EITU", 0xFF33757B },
+        { "EMCU", 0xFF33757B },
+        { "FCIU", 0xFF138F51 },
+        { "FSCU", 0xFF138F51 },
+        { "GATU", 0xFF138F51 },
+        { "GESU", 0xFFFF6600 },
+        { "GLDU", 0xFFFF6600 },
+        { "HASU", 0xFFFF6600 },
+        { "HDMU", 0xFFFF6600 },
+        { "HLBU", 0xFFFF6600 },
+        { "HLXU", 0xFFFF6600 },
+        { "HMCU", 0xFFFF6600 },
+        { "IMTU", 0xFFFF6600 },
+        { "INKU", 0xFFFF6600 },
+        { "IPXU", 0xFFFF6600 },
+        { "KKFU", 0xFF138F51 },
+        { "KKTU", 0xFF138F51 },
+        { "MAEU", 0xFFFE5429 },
+        { "MAGU", 0xFFFE5429 },
+        { "MEDU", 0xFFFE5429 },
+        { "MNBU", 0xFFFE5429 },
+        { "MOAU", 0xFFFE5429 },
+        { "MOFU", 0xFFFE5429 },
+        { "MORU", 0xFFFE5429 },
+        { "MOTU", 0xFFCE4231 },
+        { "MRKU", 0xFFCE4231 },
+        { "MSCU", 0xFF347CD0 },
+        { "MSKU", 0xFF347CD0 },
+        { "MWCU", 0xFFD32A87 },
+        { "NYKU", 0xFFD32A87 },
+        { "OOCU", 0xFFD32A87 },
+        { "OOLU", 0xFFD32A87 },
+        { "PCIU", 0xFFD32A87 },
+        { "PONU", 0xFFD32A87 },
+        { "SEGU", 0xFFCFCFCF },
+        { "SUDU", 0xFFCFCFCF },
+        { "SZLU", 0xFFD32A87 },
+        { "TCKU", 0xFFD32A87 },
+        { "TCLU", 0xFFD32A87 },
+        { "TCNU", 0xFFEAB75E },
+        { "TEMU", 0xFFEAB75E },
+        { "TGHU", 0xFFEAB75E },
+        { "TRHU", 0xFFCCCCB0 },
+        { "TRIU", 0xFF1D4092 },
+        { "TRLU", 0xFF167B42 },
+        { "TTNU", 0xFFCCCCB0 },
+        { "UACU", 0xFFCCCCB0 },
+        { "UESU", 0xFFCCCCB0 },
+        { "UETU", 0xFFCCCCB0 },
+        { "WHLU", 0xFFCCCCB0 },
+        { "XINU", 0xFFCCCCB0 },
+        { "YMLU", 0xFFCCCCB0 },
+        { "YMMU", 0xFFA2554D },
+        { "ZCSU", 0xFFA2554D },
+        { "ZIMU", 0xFFA2554D }
+    };
+}
---
+++ MagicLeap/Assets/scripts/UpdateCircleColor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using System;
+using UnityEngine.UI;
+
+
+namespace MagicLeap
+{
+    public class UpdateCircleColor : MonoBehaviour
+    {
+        //The Color to be assigned to the Renderer’s Material
+        private Color m_NewColor;
+        SpriteRenderer m_SpriteRenderer;
+
+
+        void Start()
+        {
+            m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        Color GenerateColorFromRange(float minimum, float maximum, float value)
+        {
+            float ratio = (2 * (value - minimum)) / (maximum - minimum);
+            float b = Math.Max(0f, 255f * (1f - ratio));
+            float g = Math.Max(0, 255 * (ratio - 1));
+            float r = 255f - 2 * b - 2 * g;
+            return new Color(r, g, b);
+        }
+
+        Color GetColorFromRedYellowGreenGradient(double temperatureInCelcius)
+        {
+            double percentage = (temperatureInCelcius / 70) * 100;
+            var red = (percentage > 50 ? 1 - 2 * (percentage - 50) / 100.0 : 1.0) * 255;
+            var green = (percentage > 50 ? 1.0 : 2 * percentage / 100.0) * 255;
+            var blue = 0.0;
+            Color result = new Color((float)red, (float)green, (float)blue);
+            return result;
+        }
+
+
+        /*
+         * https://stackoverflow.com/a/20793850
+         * https://www.rapidtables.com/web/color/RGB_Color.html
+        */
+        Color convert_to_rgb(double minval, double maxval, double val, Color[] colors)
+        {
+            /*
+# "colors" is a series of RGB colors delineating a series of
+# adjacent linear color gradients between each pair.
+# Determine where the given value falls proportionality within
+# the range from minval->maxval and scale that fractional value
+# by the total number in the "colors" pallette.
+            */
+            double i_f = (double)((val - minval) / (maxval - minval) * ((colors.Length) - 1));
+
+            /*
+# Determine the lower index of the pair of color indices this
+# value corresponds and its fractional distance between the lower
+# and the upper colors.
+            */
+            //# Split into whole & fractional parts.
+            int i = (int)(i_f / 1);
+            double f = i_f % 1;
+
+            //# Does it fall exactly on one of the color points?
+            if (f < (2.22045e-16))
+            {
+                return colors[i];
+            }
+            else //# Otherwise return a color within the range between them.
+            {
+                float r1 = colors[i].r;
+                float g1 = colors[i].g;
+                float b1 = colors[i].b;
+                float r2 = colors[i + 1].r;
+                float g2 = colors[i + 1].g;
+                float b2 = colors[i + 1].b;
+                Color result = new Color((float)((r1 + f * (r2 - r1)) / 255f), (float)((g1 + f * (g2 - g1)) / 255f), (float)((b1 + f * (b2 - b1))) / 255f);
+                return result;
+            }
+        }
+
+        void Update()
+        {
+            UpdateUI _UpdateUI = transform.parent.gameObject.GetComponent<UpdateUI>();
+
+            //Set the Color to the values gained from the Sliders
+            //m_NewColor = GenerateColorFromRange(0f, 50f, _UpdateUI.currentTemperature);
+            //m_NewColor = GetColorFromRedYellowGreenGradient(_UpdateUI.currentTemperature);
+
+            Color[] col = { new Color(50, 255, 100), new Color(150, 150, 50), new Color(255, 0, 0) };
+            m_NewColor = convert_to_rgb(10, 40, _UpdateUI.currentTemperature, col);
+            //Set the SpriteRenderer to the Color defined by the Sliders
+            m_SpriteRenderer.color = m_NewColor;
+        }
+    }
+}
