387b92ffa38168f501421bd5b7839e883d42158f
samerboules <samerboules@gmail.com>
2019-06-13 11:49:39 +0200

    Change ship and change the marker

---
+++ Android/2. Unity Project/Assets/OnGaze.cs
@@ -0,0 +1,19 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGaze : MonoBehaviour, IFocusable
+{
+    public void OnFocusEnter()
+    {
+        Transform StatusWindo = transform.Find("StatusWindow");
+        StatusWindo.localScale = new Vector3(6f, 6f, 6f);
+    }
+
+    public void OnFocusExit()
+    {
+        Transform StatusWindo = transform.Find("StatusWindow");
+        StatusWindo.localScale = new Vector3(6f, 6f, 6f);
+    }
+}
